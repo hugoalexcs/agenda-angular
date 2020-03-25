e5fbca1641671666365384cfbc294978fd9b8ff6
Hugo Silva <hugoengcomp@gmail.com>
2020-03-25 10:55:27 -0300

    Estamos implementando a internacionalizaçao da agenda

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,128 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+//https://stackblitz.com/edit/angular-wtifyf-upolxf?file=default.html
+//https://www.syncfusion.com/forums/145243/dynamicly-change-language-locale-of-an-date-format
+//https://github.com/syncfusion/ej-global/blob/master/i18n/ej.culture.pt-BR.min.js
+//https://stackoverflow.com/questions/57010589/how-to-set-syncfusion-components-pt-br
+import { Component, ViewEncapsulation, OnInit } from '@angular/core';
 import { scheduleData } from './data';
-import { extend } from '@syncfusion/ej2-base';
+import { L10n, loadCldr, setCulture, setCurrencyCode, extend } from '@syncfusion/ej2-base';
 import {
     EventSettingsModel, EventRenderedArgs, View, DayService, WeekService, WorkWeekService,
     MonthService, AgendaService, ResizeService, DragAndDropService
-} from '@syncfusion/ej2-angular-schedule';
+  } from '@syncfusion/ej2-angular-schedule';
+  import { orderDatas } from './data-locale';
+const  cagregorian =   './ca-gregorian.json';
+const currencies = './currencies.json';
+const numbers = './numbers.json';
+const timeZoneNames = './timeZoneNames.json';
+//setCulture('fr')
+//setCurrencyCode('BRT');
+
+loadCldr(cagregorian, currencies, numbers, timeZoneNames); // load json files
+L10n.load({
+  "en": {
+      "schedule": {
+          "day": "Dia",
+          "week": "Semana",
+          "workWeek": "Segunda a Sexta",
+          "month": "Mês",
+          "agenda": "Agenda",
+          "weekAgenda": "Agenda da Semana",
+          "workWeekAgenda": "Work Week Agenda",
+          "monthAgenda": "Month Agenda",
+          "today": "Hoje",
+          "noEvents": "No events",
+          "emptyContainer": "There are no events scheduled on this day.",
+          "allDay": "All day",
+          "start": "Start",
+          "end": "End",
+          "more": "more",
+          "close": "Close",
+          "cancel": "Cancel",
+          "noTitle": "(No Title)",
+          "delete": "Delete",
+          "deleteEvent": "Delete Event",
+          "deleteMultipleEvent": "Delete Multiple Events",
+          "selectedItems": "Items selected",
+          "deleteSeries": "Delete Series",
+          "edit": "Edit",
+          "editSeries": "Edit Series",
+          "editEvent": "Edit Event",
+          "createEvent": "Create",
+          "subject": "Subject",
+          "addTitle": "Add title",
+          "moreDetails": "More Details",
+          "save": "Save",
+          "editContent": "Do you want to edit only this event or entire series?",
+          "deleteRecurrenceContent": "Do you want to delete only this event or entire series?",
+          "deleteContent": "Are you sure you want to delete this event?",
+          "deleteMultipleContent": "Are you sure you want to delete the selected events?",
+          "newEvent": "New Event",
+          "title": "Title",
+          "location": "Location",
+          "description": "Description",
+          "timezone": "Timezone",
+          "startTimezone": "Start Timezone",
+          "endTimezone": "End Timezone",
+          "repeat": "Repeat",
+          "saveButton": "Save",
+          "cancelButton": "Cancel",
+          "deleteButton": "Delete",
+          "recurrence": "Recurrence",
+          "wrongPattern": "The recurrence pattern is not valid.",
+          "seriesChangeAlert": "The changes made to specific instances of this series will be cancelled and those events will match the series again.",
+          "createError": "The duration of the event must be shorter than how frequently it occurs. Shorten the duration, or change the recurrence pattern in the recurrence event editor.",
+          "recurrenceDateValidation": "Some months have fewer than the selected date. For these months, the occurrence will fall on the last date of the month.",
+          "sameDayAlert": "Two occurrences of the same event cannot occur on the same day.",
+          "editRecurrence": "Edit Recurrence",
+          "repeats": "Repeats",
+          "alert": "Alert",
+          "startEndError": "The selected end date occurs before the start date.",
+          "invalidDateError": "The entered date value is invalid.",
+          "ok": "Ok",
+          "occurrence": "Occurrence",
+          "series": "Series",
+          "previous": "Previous",
+          "next": "Next",
+          "timelineDay": "Timeline Day",
+          "timelineWeek": "Timeline Week",
+          "timelineWorkWeek": "Timeline Work Week",
+          "timelineMonth": "Timeline Month"
+      },
+      "recurrenceeditor": {
+          "none": "None",
+          "daily": "Daily",
+          "weekly": "Weekly",
+          "monthly": "Monthly",
+          "month": "Month",
+          "yearly": "Yearly",
+          "never": "Never",
+          "until": "Until",
+          "count": "Count",
+          "first": "First",
+          "second": "Second",
+          "third": "Third",
+          "fourth": "Fourth",
+          "last": "Last",
+          "repeat": "Repeat",
+          "repeatEvery": "Repeat Every",
+          "on": "Repeat On",
+          "end": "End",
+          "onDay": "Day",
+          "days": "Day(s)",
+          "weeks": "Week(s)",
+          "months": "Month(s)",
+          "years": "Year(s)",
+          "every": "every",
+          "summaryTimes": "time(s)",
+          "summaryOn": "on",
+          "summaryUntil": "until",
+          "summaryRepeat": "Repeats",
+          "summaryDay": "day(s)",
+          "summaryWeek": "week(s)",
+          "summaryMonth": "month(s)",
+          "summaryYear": "year(s)"
+      }
+}});
 
 @Component({
     selector: 'app-root',
@@ -22,12 +140,28 @@ import {
     providers: [DayService, WeekService, WorkWeekService, MonthService, AgendaService, ResizeService, DragAndDropService],
     encapsulation: ViewEncapsulation.None
 })
-export class AppComponent {
+export class AppComponent  implements OnInit  {
+
+  
+
     public data: Object[] = [] = <Object[]>extend([], scheduleData, null, true);
     public selectedDate: Date = new Date(2019, 0, 10);
     public eventSettings: EventSettingsModel = { dataSource: this.data };
     public currentView: View = 'Week';
+    public views: Array<string> = ['Day', 'Week', 'WorkWeek', 'Month'];
+  //locale='pt-Br' 
+  public formatoptions: Object;
+  public cultureChange = false;
+  ngOnInit(): void {
+    this.data = orderDatas;
+    this.formatoptions = { type: 'dateTime', format: 'M/d/y' }
 
+    this.cultureChange = true;
+    setCulture('pt'); // Change the Grid culture
+   setCurrencyCode('BRL');// Change the currency code
+
+  }
+  
     onClear(event): void {
         document.getElementById('EventLog').innerHTML = '';
     }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { registerLocaleData, CommonModule } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
 import {ScheduleModule, AgendaService, DayService, DragAndDropService, ResizeService, WeekService, WorkWeekService, MonthService } from '@syncfusion/ej2-angular-schedule';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+registerLocaleData(localePt, 'pt-PT');
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+//import {HttpClientModule, HttpClientJsonpModule} from "@angular/common/http";
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+/*     HttpClientModule,
+    HttpClientJsonpModule, */
+    ReactiveFormsModule, FormsModule,
     BrowserModule,
     AppRoutingModule,
     ScheduleModule
   ],
-  providers: [AgendaService, DayService, WeekService, WorkWeekService, MonthService, DragAndDropService, ResizeService],
+  providers: [{ provide: LOCALE_ID, useValue: 'pt-PT' } ,
+    AgendaService, DayService, WeekService, WorkWeekService, MonthService, DragAndDropService, ResizeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
